63b77d13f85394bda02fb71b118bb407bdd8547e
Kristof-nl <70227336+Kristof-nl@users.noreply.github.com>
2021-11-08 19:44:39 +0100

    Add ReverseString.cs

---
+++ Exercises/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] reversearray = new char[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char temp = input[(input.Length - 1) - i];
+            reversearray[i] = temp;
+        }
+        string reverseString = new string(reversearray);
+        return reverseString;
+    }
+}
