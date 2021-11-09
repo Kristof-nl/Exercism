ced28fff062707f8c0e7aa931a6ef1de581a935d
Kristof-nl <70227336+Kristof-nl@users.noreply.github.com>
2021-11-09 23:16:41 +0100

    Add Tim_from_marketing

---
+++ Exercises/Tim_from_marketing.cs
@@ -0,0 +1,28 @@
+using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        if (id != null && department != null)
+        {
+            return "[" + id + "] - " + name + " - " + department.ToUpper();
+        }
+        else if ((id == null && department != null))
+        {
+            return name + " - " + department.ToUpper();
+        }
+        else if ((id == null && department == null))
+        {
+            return name + " - OWNER";
+        }
+        else if ((id != null && department == null))
+        {
+            return "[" + id + "] - " + name + " - OWNER";
+        }
+        else
+        {
+            return "Error";
+        }
+    }
+}
