d92422a2594f11488b30a6f234b851f948cf0ef2
Kristof-nl <70227336+Kristof-nl@users.noreply.github.com>
2021-11-10 17:43:58 +0100

    Add Darts.cs

---
+++ Exercises/Darts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+    double circlePoint  = Math.Pow(x, 2) + Math.Pow(y, 2);
+       if (circlePoint <= 1)
+       {
+           return 10;
+       }
+       else if (circlePoint > 1 && circlePoint <= 25)
+        {
+            return 5;
+        }
+         else if (circlePoint > 25 && circlePoint <= 100)
+        {
+            return 1;
+        }
+       else 
+       {
+           return 0;
+       }
+    }
+}
